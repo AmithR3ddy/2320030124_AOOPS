2b259803d74ce6fc16c1f0c89797b8ce08bcb022
AmithR3ddy <144897340+AmithR3ddy@users.noreply.github.com>
2024-08-28 09:27:32 +0530

    Delete JavaProject directory

--- JavaProject/LearningPlatform/src/ELearningPlatform/CertificateIssuer.java
+++
@@ -1,8 +0,0 @@
-package ELearningPlatform;
-
-public class CertificateIssuer {
-    public void issueCertificate(User user, Course course) {
-        System.out.println("Certificate issued to " + user.getName() + " for completing the course " + course.getCourseName());
-    }
-}
-
